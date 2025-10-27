9ca1b224c31a71f7864eb222ca5bd086e61a4136
Spoon <121692985+SpoonWasAlreadyTaken@users.noreply.github.com>
2025-10-27 17:00:30 +0200

    2.0.1

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,10 +1,17 @@
 local builtin = require('telescope.builtin')
+local telescope = require('telescope')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
-
+telescope.setup({
+    defaults = {
+        file_ignore_patterns = {
+            "orgsd/",
+        },
+    },
+})
 
 
