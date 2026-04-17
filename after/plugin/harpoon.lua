6a6727555964fa9006bb8e20b44ed7d88f13df8e
Spoon <121692985+SpoonWasAlreadyTaken@users.noreply.github.com>
2026-04-17 15:53:19 +0300

    Update harpoon.lua

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,5 +1,19 @@
 local harpoon = require("harpoon")
-harpoon:setup()
+harpoon:setup({
+    settings = {
+        save_on_toggle = true,
+        sync_on_ui_close = true,
+    },
+    default = {
+        mappings = {
+            list = {
+                ["dd"] = function(ctx)
+                    harpoon:list():remove_at(ctx.item_idx)
+            end,
+        },
+    },
+},
+})
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
