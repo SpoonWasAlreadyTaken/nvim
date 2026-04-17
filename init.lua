add1ae152a6909577e849c217e45bd68547e38e5
Spoon <121692985+SpoonWasAlreadyTaken@users.noreply.github.com>
2026-04-17 15:28:09 +0300

    fixed root detection

--- init.lua
+++ init.lua
@@ -1,1 +1,14 @@
 require("spoon")
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function()
+        local filepath = vim.api.nvim_buf_get_name(0)
+        if filepath == "" then return end
+
+        local root = vim.fs.root(filepath, { ".git", "*.cpp", "*.c", "*.hpp" ,"*.h", "*.lua" })
+
+        if root then
+            vim.fn.chdir(root)
+        end
+    end,
+})
